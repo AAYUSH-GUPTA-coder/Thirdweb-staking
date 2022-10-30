c0c9b80fb30132cd47648cecad3e290b85dad21b
AAYUSH-GUPTA-coder <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2022-10-30 13:37:48 +0530

    Update NftStaking.sol

--- thirdweb-staking/NftStaking.sol
+++ thirdweb-staking/NftStaking.sol
@@ -19,7 +19,7 @@ contract ERC721Staking is ReentrancyGuard {
         rewardToken = _rewardToken;
     }
 
-    struct StakeToken {
+    struct StakedToken {
         address staker;
         address tokenId;
     }
@@ -41,7 +41,7 @@ contract ERC721Staking is ReentrancyGuard {
     }
 
     // Rewards per hour per token deposited in wei.
-    uint256 private rewardsPerHour = 100000;
+    uint256 private rewardsPerHour = 100000;  // 0.0000000000001 ETH
 
     // Mapping of User Address to Staker info
     mapping(address => Staker) public stakers;
@@ -85,4 +85,113 @@ contract ERC721Staking is ReentrancyGuard {
         // Update the timeOfLastUpdate for the staker   
         stakers[msg.sender].timeOfLastUpdate = block.timestamp;
     }
+
+    // Check if user has any ERC721 Tokens Staked and if they tried to withdraw,
+    // calculate the rewards and store them in the unclaimedRewards
+    // decrement the amountStaked of the user and transfer the ERC721 token back to them
+    function withdraw(uint256 _tokenId) external nonReentrant {
+        // Make sure the user has at least one token staked before withdrawing
+        require(
+            stakers[msg.sender].amountStaked > 0,
+            "You have no tokens staked"
+        );
+        
+        // Wallet must own the token they are trying to withdraw
+        require(stakerAddress[_tokenId] == msg.sender, "You don't own this token!");
+
+        // Update the rewards for this user, as the amount of rewards decreases with less tokens.
+        uint256 rewards = calculateRewards(msg.sender);
+        stakers[msg.sender].unclaimedRewards += rewards;
+
+        // Find the index of this token id in the stakedTokens array
+        uint256 index = 0;
+        for (uint256 i = 0; i < stakers[msg.sender].stakedTokens.length; i++) {
+            if (
+                stakers[msg.sender].stakedTokens[i].tokenId == _tokenId 
+                && 
+                stakers[msg.sender].stakedTokens[i].staker != address(0)
+            ) {
+                index = i;
+                break;
+            }
+        }
+
+        // Set this token's .staker to be address 0 to mark it as no longer staked
+        stakers[msg.sender].stakedTokens[index].staker = address(0);
+
+        // Decrement the amount staked for this wallet
+        stakers[msg.sender].amountStaked--;
+
+        // Update the mapping of the tokenId to the be address(0) to indicate that the token is no longer staked
+        stakerAddress[_tokenId] = address(0);
+
+        // Transfer the token back to the withdrawer
+        nftCollection.transferFrom(address(this), msg.sender, _tokenId);
+
+        // Update the timeOfLastUpdate for the withdrawer   
+        stakers[msg.sender].timeOfLastUpdate = block.timestamp;
+    }
+
+    // Calculate rewards for the msg.sender, check if there are any rewards
+    // claim, set unclaimedRewards to 0 and transfer the ERC20 Reward token
+    // to the user.
+    function claimRewards() external {
+        uint256 rewards = calculateRewards(msg.sender) +
+            stakers[msg.sender].unclaimedRewards;
+        require(rewards > 0, "You have no rewards to claim");
+        stakers[msg.sender].timeOfLastUpdate = block.timestamp;
+        stakers[msg.sender].unclaimedRewards = 0;
+        rewardsToken.safeTransfer(msg.sender, rewards);
+    }
+
+
+    //////////
+    // View //
+    //////////
+
+    function availableRewards(address _staker) public view returns (uint256) {
+        uint256 rewards = calculateRewards(_staker) +
+            stakers[_staker].unclaimedRewards;
+        return rewards;
+    }
+
+    function getStakedTokens(address _user) public view returns (StakedToken[] memory) {
+        // Check if we know this user
+        if (stakers[_user].amountStaked > 0) {
+            // Return all the tokens in the stakedToken Array for this user that are not -1
+            StakedToken[] memory _stakedTokens = new StakedToken[](stakers[_user].amountStaked);
+            uint256 _index = 0;
+
+            for (uint256 j = 0; j < stakers[_user].stakedTokens.length; j++) {
+                if (stakers[_user].stakedTokens[j].staker != (address(0))) {
+                    _stakedTokens[_index] = stakers[_user].stakedTokens[j];
+                    _index++;
+                }
+            }
+
+            return _stakedTokens;
+        }
+        
+        // Otherwise, return empty array
+        else {
+            return new StakedToken[](0);
+        }
+    }
+
+    // Internal
+
+    // Calculate rewards for param _staker by calculating the time passed
+    // since last update in hours and mulitplying it to ERC721 Tokens Staked
+    // and rewardsPerHour.
+    function calculateRewards(address _staker)
+        internal
+        view
+        returns (uint256 _rewards)
+    {
+        return (((
+            ((block.timestamp - stakers[_staker].timeOfLastUpdate) *
+                stakers[_staker].amountStaked)
+        ) * rewardsPerHour) / 3600);
+    }
+}
 }
